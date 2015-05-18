57d4c447131f995a8ad9d88f9003650169e70efd
ra135238 <carlacremon@gmail.com>
2015-05-18 14:34:32 -0300

    Create Atendente.java

---
+++ Atendente.java
@@ -0,0 +1,25 @@
+/**
+ */
+package Model;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Atendente</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see Model.ModelPackage#getAtendente()
+ * @model
+ * @generated
+ */
+public interface Atendente extends Funcionario {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @model
+	 * @generated
+	 */
+	void AutorizaEmprestimo();
+
+} // Atendente
