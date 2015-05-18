97e1d05cc24ce2e9988110806c8529b2d81fbc06
ra135238 <carlacremon@gmail.com>
2015-05-18 14:34:02 -0300

    Create Bibliotecario.java

---
+++ Bibliotecario.java
@@ -0,0 +1,25 @@
+/**
+ */
+package Model;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Bibliotecario</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see Model.ModelPackage#getBibliotecario()
+ * @model
+ * @generated
+ */
+public interface Bibliotecario extends Funcionario {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @model
+	 * @generated
+	 */
+	void ManipulaExemplar();
+
+} // Bibliotecario
